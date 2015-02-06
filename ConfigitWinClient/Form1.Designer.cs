05a391daa82b75d33df155910365d9ada167c835
Saurabh Garg <saurabh.garg@nagarro.com>
2015-02-06 17:40:39 -0500

    New Change

---
+++ ConfigitWinClient/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ConfigitWinClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblColor = new System.Windows.Forms.Label();
+            this.lblPrint = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            this.cbPrint = new System.Windows.Forms.ComboBox();
+            this.cbSize = new System.Windows.Forms.ComboBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(37, 55);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(31, 13);
+            this.lblColor.TabIndex = 0;
+            this.lblColor.Text = "Color";
+            // 
+            // lblPrint
+            // 
+            this.lblPrint.AutoSize = true;
+            this.lblPrint.Location = new System.Drawing.Point(37, 88);
+            this.lblPrint.Name = "lblPrint";
+            this.lblPrint.Size = new System.Drawing.Size(28, 13);
+            this.lblPrint.TabIndex = 1;
+            this.lblPrint.Text = "Print";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(37, 118);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(27, 13);
+            this.lblSize.TabIndex = 2;
+            this.lblSize.Text = "Size";
+            // 
+            // cbColor
+            // 
+            this.cbColor.FormattingEnabled = true;
+            this.cbColor.Location = new System.Drawing.Point(74, 52);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(95, 21);
+            this.cbColor.TabIndex = 3;
+            this.cbColor.SelectedIndexChanged += new System.EventHandler(this.cbColor_SelectedIndexChanged);
+            // 
+            // cbPrint
+            // 
+            this.cbPrint.FormattingEnabled = true;
+            this.cbPrint.Location = new System.Drawing.Point(74, 88);
+            this.cbPrint.Name = "cbPrint";
+            this.cbPrint.Size = new System.Drawing.Size(95, 21);
+            this.cbPrint.TabIndex = 4;
+            this.cbPrint.SelectedIndexChanged += new System.EventHandler(this.cbPrint_SelectedIndexChanged);
+            // 
+            // cbSize
+            // 
+            this.cbSize.FormattingEnabled = true;
+            this.cbSize.Location = new System.Drawing.Point(74, 118);
+            this.cbSize.Name = "cbSize";
+            this.cbSize.Size = new System.Drawing.Size(95, 21);
+            this.cbSize.TabIndex = 5;
+            this.cbSize.SelectedIndexChanged += new System.EventHandler(this.cbSize_SelectedIndexChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(74, 160);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.cbSize);
+            this.Controls.Add(this.cbPrint);
+            this.Controls.Add(this.cbColor);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblPrint);
+            this.Controls.Add(this.lblColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label lblPrint;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.ComboBox cbColor;
+        private System.Windows.Forms.ComboBox cbPrint;
+        private System.Windows.Forms.ComboBox cbSize;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ ConfigitWinClient/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Configit.Runtime;
+
+
+namespace ConfigitWinClient
+{
+    public partial class Form1 : Form
+    {
+
+        ProductModel model = null;
+        public Form1()
+        {
+            InitializeComponent();
+            model = ProductModel.CreateFromVT("SaveTheWhales.vtz");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDropDowns();
+
+        }
+
+        private void LoadDropDowns()
+        {
+            
+
+            cbColor.SelectedIndexChanged -= cbColor_SelectedIndexChanged;
+            cbPrint.SelectedIndexChanged -= cbPrint_SelectedIndexChanged;
+            cbSize.SelectedIndexChanged -= cbSize_SelectedIndexChanged;
+
+            cbColor.Items.Clear();
+
+
+            cbColor.Text = string.Empty;
+            cbColor.Items.AddRange(model.AllVariables["color"].Values.ToArray());
+
+            cbPrint.Items.Clear();
+
+            cbPrint.Text = string.Empty;
+            cbPrint.Items.AddRange(model.AllVariables["print"].Values.ToArray());
+
+            cbSize.Items.Clear();
+            cbSize.Text = string.Empty;
+            cbSize.Items.AddRange(model.AllVariables["size"].Values.ToArray());
+
+            cbColor.SelectedIndexChanged += cbColor_SelectedIndexChanged;
+            cbPrint.SelectedIndexChanged += cbPrint_SelectedIndexChanged;
+            cbSize.SelectedIndexChanged += cbSize_SelectedIndexChanged;
+
+        }
+
+        private void cbColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            model.AllVariables["color"].Assign(cbColor.SelectedItem.ToString());
+            SetAssignment();
+        }
+
+        private void cbPrint_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            model.AllVariables["print"].Assign(cbPrint.SelectedItem.ToString());
+            SetAssignment();
+        }
+
+        private void cbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            model.AllVariables["size"].Assign(cbSize.SelectedItem.ToString());
+            SetAssignment();
+        }
+
+        void SetAssignment()
+        {
+            cbColor.SelectedIndexChanged -= cbColor_SelectedIndexChanged;
+            cbPrint.SelectedIndexChanged -= cbPrint_SelectedIndexChanged;
+            cbSize.SelectedIndexChanged -= cbSize_SelectedIndexChanged;
+
+
+            //cbPrint.Items.Clear();
+            //var legealValues = model.AllVariables["print"].GetLegalValueString();
+            //var valueCollections = legealValues.Split(',');
+            cbPrint.SelectedItem = null;
+            cbPrint.SelectedItem = model.AllVariables["print"].AssignedValue;
+
+            cbColor.SelectedItem = null;
+            cbColor.SelectedItem = model.AllVariables["color"].AssignedValue;
+
+            cbSize.SelectedItem = null;
+            cbSize.SelectedItem = model.AllVariables["size"].AssignedValue;
+
+            cbColor.SelectedIndexChanged += cbColor_SelectedIndexChanged;
+            cbPrint.SelectedIndexChanged += cbPrint_SelectedIndexChanged;
+            cbSize.SelectedIndexChanged += cbSize_SelectedIndexChanged;
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            model.Reset();
+            LoadDropDowns();
+        }
+
+    }
+}
